307d6c666e1f690de44ee6e3bb87cf90af37bb10
TJonCanon <taylor.joyner93@me.com>
2024-06-25 18:02:59 -0500

    task 3

---
+++ 0x03-asynchronous/3-great_user.dart
@@ -0,0 +1,28 @@
+import 'dart:convert';
+import '3-util.dart';
+
+Future<String> greetUser() async {
+  try {
+    String userData = await fetchUserData();
+    Map<String, dynamic> user = json.decode(userData);
+    String username = user['username'];
+    return 'Hello $username';
+  } catch (error) {
+    return 'error caught: $error';
+  }
+}
+
+Future<String> loginUser() async {
+  try {
+    bool isLoggedIn = await checkCredentials();
+    print('There is a user: $isLoggedIn');
+
+    if (isLoggedIn) {
+      return await greetUser();
+    } else {
+      return 'Wrong credentials';
+    }
+  } catch (error) {
+    return 'error caught: $error';
+  }
+}
