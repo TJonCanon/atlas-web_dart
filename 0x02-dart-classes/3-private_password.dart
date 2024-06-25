08cb1535840ab3cd842dc21d1c4e30697665b6a3
TJonCanon <taylor.joyner93@me.com>
2024-06-25 15:20:01 -0500

    task 3

---
+++ 0x02-dart-classes/3-private_password.dart
@@ -0,0 +1,33 @@
+class Password {
+  String _password;
+
+  Password({required String password}) : _password = password;
+
+  bool isValid() {
+    if (_password.length < 8 || _password.length > 16) {
+      return false;
+    }
+
+    bool hasUppercase = false;
+    bool hasLowercase = false;
+    bool hasNumber = false;
+
+    for (int i = 0; i < _password.length; i++) {
+      String char = _password[i];
+      if (char.toUpperCase() == char && char.toLowerCase() != char) {
+        hasUppercase = true;
+      } else if (char.toLowerCase() == char && char.toUpperCase() != char) {
+        hasLowercase = true;
+      } else if (int.tryParse(char) != null) {
+        hasNumber = true;
+      }
+    }
+
+    return hasUppercase && hasLowercase && hasNumber;
+  }
+
+  @override
+  String toString() {
+    return 'Your Password is $_password';
+  }
+}
