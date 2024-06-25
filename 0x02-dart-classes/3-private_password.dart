937d9dfe58e1ac616b730210b4f814e246f2446d
TJonCanon <taylor.joyner93@me.com>
2024-06-25 15:28:19 -0500

    task 3

--- 0x02-dart-classes/3-private_password.dart
+++ 0x02-dart-classes/3-private_password.dart
@@ -1,33 +1,20 @@
 class Password {
-  String _password;
-
-  Password({required String password}) : _password = password;
-
-  bool isValid() {
-    if (_password.length < 8 || _password.length > 16) {
-      return false;
-    }
-
-    bool hasUppercase = false;
-    bool hasLowercase = false;
-    bool hasNumber = false;
-
-    for (int i = 0; i < _password.length; i++) {
-      String char = _password[i];
-      if (char.toUpperCase() == char && char.toLowerCase() != char) {
-        hasUppercase = true;
-      } else if (char.toLowerCase() == char && char.toUpperCase() != char) {
-        hasLowercase = true;
-      } else if (int.tryParse(char) != null) {
-        hasNumber = true;
-      }
-    }
-
-    return hasUppercase && hasLowercase && hasNumber;
-  }
+  String _password = '';
 
   @override
   String toString() {
-    return 'Your Password is $_password';
+    return 'Your Password is: $_password';
+  }
+
+  bool isValid() {
+    return _password.length >= 8 &&
+        _password.length <= 16 &&
+        _password.contains(RegExp(r'[A-Z]')) &&
+        _password.contains(RegExp(r'[a-z]')) &&
+        _password.contains(RegExp(r'[0-9]'));
+  }
+
+  Password({required String password}) {
+    _password = password;
   }
 }
